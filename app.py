9b18874c1e3a9ccd347c93043aae956f807102a2
yuu-eguci <yuu.eguci@gmail.com>
2019-08-20 14:23:10 +0900

    Run with different setting on Heroku from on local

--- app.py
+++ app.py
@@ -14,8 +14,14 @@ def tpl():
         'bbbb': 'bbbb',
     }
 
-run(host='0.0.0.0',
-    port=int(os.environ.get('PORT', 5000)),
-    reloader=True,  # Enable auto reload.
-    debug=True,     # Display error detail on browser.
-    )
+# Run with different setting on Heroku from on local.
+if os.environ.get('APP_LOCATION') == 'heroku':
+    run(host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000)),
+        )
+else:
+    run(host='0.0.0.0',
+        port=8080,
+        reloader=True,  # Enable auto reload.
+        debug=True,     # Display error detail on browser.
+        )
