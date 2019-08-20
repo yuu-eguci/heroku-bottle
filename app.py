0b664fbfbaaa05f4a34556cdd6e09b8ae6d8c7b9
yuu-eguci <yuu.eguci@gmail.com>
2019-08-20 14:07:32 +0900

    Add a page using template

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 
 import os
-from bottle import route, run
+from bottle import route, run, view
 
 @route('/')
 def hello_world():
     return 'Hello World!'
 
-run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+@route('/tpl')
+@view('tpl')
+def tpl():
+    return {
+        'aaaa': 'aaaa',
+        'bbbb': 'bbbb',
+    }
+
+run(host='0.0.0.0',
+    port=int(os.environ.get('PORT', 5000)),
+    reloader=True,  # Enable auto reload.
+    debug=True,     # Display error detail on browser.
+    )
